cae82b188c2a3b494f1c968edffa3ab1bd0e028f
diegofreitas09 <diegofreitas090707@gmail.com>
2026-08-21 01:26:23 -0300

    Configura Firebase no app web

---
+++ src/lib/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB-GIQBnXx-oP4pG400PuDmvxWxpkTigZk",
+  authDomain: "pdf-concurso-edu.firebaseapp.com",
+  projectId: "pdf-concurso-edu",
+  storageBucket: "pdf-concurso-edu.firebasestorage.app",
+  messagingSenderId: "314121207816",
+  appId: "1:314121207816:web:4a32b3f04f8e4ae58651bd",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export default app;
