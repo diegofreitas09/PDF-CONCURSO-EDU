2278e24144b2ed8c79ce41006b682bd91b287c70
diegofreitas09 <diegofreitas090707@gmail.com>
2026-08-26 19:04:08 -0300

    feat: integra lote pedagogico da apostila presencial

--- src/data/questionSources/index.js
+++ src/data/questionSources/index.js
@@ -1,6 +1,7 @@
 import { QUESTION_BANK as LEGACY_QUESTION_BANK } from "../questionBank";
 import { SIMULADO_05_TEORIAS_PEDAGOGICAS } from "./simulado05TeoriasPedagogicas";
 import { SEDUC_CONHECIMENTOS_GERAIS_MODULO_1 } from "./seducConhecimentosGeraisModulo1";
+import { APOSTILA_PRESENCIAL_SEDUC_PEDAGOGICOS_01 } from "./apostilaPresencialSeducPedagogicos01";
 
 /**
  * Banco unificado da plataforma.
@@ -11,11 +12,13 @@ export const ALL_QUESTIONS = [
   ...LEGACY_QUESTION_BANK,
   ...SIMULADO_05_TEORIAS_PEDAGOGICAS,
   ...SEDUC_CONHECIMENTOS_GERAIS_MODULO_1,
+  ...APOSTILA_PRESENCIAL_SEDUC_PEDAGOGICOS_01,
 ];
 
 export const QUESTION_SOURCE_STATS = {
   legado: LEGACY_QUESTION_BANK.length,
   simulado05TeoriasPedagogicas: SIMULADO_05_TEORIAS_PEDAGOGICAS.length,
   seducConhecimentosGeraisModulo1: SEDUC_CONHECIMENTOS_GERAIS_MODULO_1.length,
-  total: LEGACY_QUESTION_BANK.length + SIMULADO_05_TEORIAS_PEDAGOGICAS.length + SEDUC_CONHECIMENTOS_GERAIS_MODULO_1.length,
+  apostilaPresencialSeducPedagogicos01: APOSTILA_PRESENCIAL_SEDUC_PEDAGOGICOS_01.length,
+  total: LEGACY_QUESTION_BANK.length + SIMULADO_05_TEORIAS_PEDAGOGICAS.length + SEDUC_CONHECIMENTOS_GERAIS_MODULO_1.length + APOSTILA_PRESENCIAL_SEDUC_PEDAGOGICOS_01.length,
 };
