5ffb7b0bc1f731a7f6e864ecad085a3dbfb30d67
diegofreitas09 <diegofreitas090707@gmail.com>
2026-08-26 19:59:52 -0300

    style: carregar refinamento visual global

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { RouterProvider } from "react-router/dom";
 import router from "./routes/router";
 import { AuthProvider } from "./context/AuthContext";
 import "./styles/global.css";
+import "./styles/uiRefinement.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
