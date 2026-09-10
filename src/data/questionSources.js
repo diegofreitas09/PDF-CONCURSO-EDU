d7f07832930141af84b43d7c66d26452503fb957
diegofreitas09 <diegofreitas090707@gmail.com>
2026-09-10 19:56:45 -0300

    feat: integrar lote UECE de Matemática ao banco global

--- src/data/questionSources.js
+++ src/data/questionSources.js
@@ -1,9 +1,11 @@
 import { ALL_QUESTIONS as RAW_ALL_QUESTIONS } from "./questionSources/index.js";
+import { UECE_MATEMATICA_LOTE_20 } from "./questionSources/ueceMatematicaLote120.js";
 import { auditQuestionBank } from "./questionAuditEngine.js";
 
 export * from "./questionSources/index.js";
 
-export const GLOBAL_QUESTION_AUDIT=auditQuestionBank(RAW_ALL_QUESTIONS);
+const RAW_WITH_UECE=[...RAW_ALL_QUESTIONS,...UECE_MATEMATICA_LOTE_20];
+export const GLOBAL_QUESTION_AUDIT=auditQuestionBank(RAW_WITH_UECE);
 export const ALL_QUESTIONS=GLOBAL_QUESTION_AUDIT.published;
 export const OCR_QUARANTINED_QUESTIONS=GLOBAL_QUESTION_AUDIT.quarantined;
 export const GLOBAL_DUPLICATE_QUESTIONS=GLOBAL_QUESTION_AUDIT.duplicates;
