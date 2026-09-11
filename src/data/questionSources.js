e28480b430504ace02245b95d63617a7fb4884f8
diegofreitas09 <diegofreitas090707@gmail.com>
2026-09-10 21:47:32 -0300

    feat: integrar lote inicial de Dinâmica UECE ao banco global

--- src/data/questionSources.js
+++ src/data/questionSources.js
@@ -4,11 +4,12 @@ import { UECE_MATEMATICA_LOTE_20 } from "./questionSources/ueceMatematicaLote120
 import { UECE_FISICA_ANALISE_DIMENSIONAL_LOTE_27 } from "./questionSources/ueceFisicaAnaliseDimensionalLote27.js";
 import { UECE_FISICA_CINEMATICA_LOTE_18 } from "./questionSources/ueceFisicaCinematicaLote18.js";
 import { UECE_FISICA_CINEMATICA_LOTE_09 } from "./questionSources/ueceFisicaCinematicaLote09.js";
+import { UECE_FISICA_DINAMICA_LOTE_03 } from "./questionSources/ueceFisicaDinamicaLote03.js";
 import { auditQuestionBank } from "./questionAuditEngine.js";
 
 export * from "./questionSources/index.js";
 
-const RAW_WITH_NEW_BATCHES=[...RAW_ALL_QUESTIONS,...SEDUC_P1_LOTE_300_02,...UECE_MATEMATICA_LOTE_20,...UECE_FISICA_ANALISE_DIMENSIONAL_LOTE_27,...UECE_FISICA_CINEMATICA_LOTE_18,...UECE_FISICA_CINEMATICA_LOTE_09];
+const RAW_WITH_NEW_BATCHES=[...RAW_ALL_QUESTIONS,...SEDUC_P1_LOTE_300_02,...UECE_MATEMATICA_LOTE_20,...UECE_FISICA_ANALISE_DIMENSIONAL_LOTE_27,...UECE_FISICA_CINEMATICA_LOTE_18,...UECE_FISICA_CINEMATICA_LOTE_09,...UECE_FISICA_DINAMICA_LOTE_03];
 export const GLOBAL_QUESTION_AUDIT=auditQuestionBank(RAW_WITH_NEW_BATCHES);
 export const ALL_QUESTIONS=GLOBAL_QUESTION_AUDIT.published;
 export const OCR_QUARANTINED_QUESTIONS=GLOBAL_QUESTION_AUDIT.quarantined;
