9d4752eea26eb912b6d743b80dff7d5b9288a1ed
diegofreitas09 <diegofreitas090707@gmail.com>
2026-09-10 21:24:24 -0300

    feat: integrar lote UECE de Cinemática ao banco global

--- src/data/questionSources.js
+++ src/data/questionSources.js
@@ -2,11 +2,12 @@ import { ALL_QUESTIONS as RAW_ALL_QUESTIONS } from "./questionSources/index.js";
 import { SEDUC_P1_LOTE_300_02 } from "./questionSources/seducP1Lote300_02.js";
 import { UECE_MATEMATICA_LOTE_20 } from "./questionSources/ueceMatematicaLote120.js";
 import { UECE_FISICA_ANALISE_DIMENSIONAL_LOTE_27 } from "./questionSources/ueceFisicaAnaliseDimensionalLote27.js";
+import { UECE_FISICA_CINEMATICA_LOTE_18 } from "./questionSources/ueceFisicaCinematicaLote18.js";
 import { auditQuestionBank } from "./questionAuditEngine.js";
 
 export * from "./questionSources/index.js";
 
-const RAW_WITH_NEW_BATCHES=[...RAW_ALL_QUESTIONS,...SEDUC_P1_LOTE_300_02,...UECE_MATEMATICA_LOTE_20,...UECE_FISICA_ANALISE_DIMENSIONAL_LOTE_27];
+const RAW_WITH_NEW_BATCHES=[...RAW_ALL_QUESTIONS,...SEDUC_P1_LOTE_300_02,...UECE_MATEMATICA_LOTE_20,...UECE_FISICA_ANALISE_DIMENSIONAL_LOTE_27,...UECE_FISICA_CINEMATICA_LOTE_18];
 export const GLOBAL_QUESTION_AUDIT=auditQuestionBank(RAW_WITH_NEW_BATCHES);
 export const ALL_QUESTIONS=GLOBAL_QUESTION_AUDIT.published;
 export const OCR_QUARANTINED_QUESTIONS=GLOBAL_QUESTION_AUDIT.quarantined;
