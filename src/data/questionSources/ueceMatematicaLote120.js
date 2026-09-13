500734e27fed010c7ef35c173e29fc6a6d9036e3
diegofreitas09 <diegofreitas090707@gmail.com>
2026-09-13 02:49:22 -0300

    Conecta Matemática UECE 132-139 ao banco auditado

--- src/data/questionSources/ueceMatematicaLote120.js
+++ src/data/questionSources/ueceMatematicaLote120.js
@@ -6,6 +6,7 @@ import { UECE_MATEMATICA_COMPLEMENTO_81_100 } from "./ueceMatematicaComplemento8
 import { UECE_MATEMATICA_COMPLEMENTO_101_110 } from "./ueceMatematicaComplemento101a110.js";
 import { UECE_MATEMATICA_COMPLEMENTO_111_121 } from "./ueceMatematicaComplemento111a121.js";
 import { UECE_MATEMATICA_POLINOMIOS_COMPLEXOS_122_131 } from "./ueceMatematicaPolinomiosComplexos122a131.js";
+import { UECE_MATEMATICA_POLINOMIOS_COMPLEXOS_132_139 } from "./ueceMatematicaPolinomiosComplexos132a139.js";
 
 // UECE por Assunto - Matemática auditada: primeiros 100 itens.
 export const UECE_MATEMATICA_LOTE_100 = [
@@ -16,12 +17,13 @@ export const UECE_MATEMATICA_LOTE_100 = [
   ...UECE_MATEMATICA_COMPLEMENTO_81_100,
 ];
 
-export const UECE_MATEMATICA_LOTE_131 = [
+export const UECE_MATEMATICA_LOTE_139 = [
   ...UECE_MATEMATICA_LOTE_100,
   ...UECE_MATEMATICA_COMPLEMENTO_101_110,
   ...UECE_MATEMATICA_COMPLEMENTO_111_121,
   ...UECE_MATEMATICA_POLINOMIOS_COMPLEXOS_122_131,
+  ...UECE_MATEMATICA_POLINOMIOS_COMPLEXOS_132_139,
 ];
 
 // Alias preservado porque o agregador legado ainda importa este nome.
-export const UECE_MATEMATICA_LOTE_20 = UECE_MATEMATICA_LOTE_131;
+export const UECE_MATEMATICA_LOTE_20 = UECE_MATEMATICA_LOTE_139;
