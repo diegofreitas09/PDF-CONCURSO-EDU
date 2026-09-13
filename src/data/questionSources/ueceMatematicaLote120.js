fa01f512d24e1754ddcae18cc2458115fb11f776
diegofreitas09 <diegofreitas090707@gmail.com>
2026-09-13 00:27:10 -0300

    Conecta Matemática UECE 101-110 ao banco auditado

--- src/data/questionSources/ueceMatematicaLote120.js
+++ src/data/questionSources/ueceMatematicaLote120.js
@@ -3,8 +3,9 @@ import { UECE_MATEMATICA_COMPLEMENTO_21_40 } from "./ueceMatematicaComplemento21
 import { UECE_MATEMATICA_COMPLEMENTO_41_60 } from "./ueceMatematicaComplemento41a60.js";
 import { UECE_MATEMATICA_COMPLEMENTO_61_80 } from "./ueceMatematicaComplemento61a80.js";
 import { UECE_MATEMATICA_COMPLEMENTO_81_100 } from "./ueceMatematicaComplemento81a100.js";
+import { UECE_MATEMATICA_COMPLEMENTO_101_110 } from "./ueceMatematicaComplemento101a110.js";
 
-// UECE por Assunto - Matemática auditada: 100 itens.
+// UECE por Assunto - Matemática auditada: primeiros 100 itens.
 export const UECE_MATEMATICA_LOTE_100 = [
   ...UECE_MATEMATICA_BASE_20,
   ...UECE_MATEMATICA_COMPLEMENTO_21_40,
@@ -13,5 +14,11 @@ export const UECE_MATEMATICA_LOTE_100 = [
   ...UECE_MATEMATICA_COMPLEMENTO_81_100,
 ];
 
+// Ampliação auditada de Matemática: 110 itens conectados ao banco.
+export const UECE_MATEMATICA_LOTE_110 = [
+  ...UECE_MATEMATICA_LOTE_100,
+  ...UECE_MATEMATICA_COMPLEMENTO_101_110,
+];
+
 // Alias preservado porque o agregador legado ainda importa este nome.
-export const UECE_MATEMATICA_LOTE_20 = UECE_MATEMATICA_LOTE_100;
+export const UECE_MATEMATICA_LOTE_20 = UECE_MATEMATICA_LOTE_110;
